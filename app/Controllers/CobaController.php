590f18315f73927b375fc5132d368916a5bf5380
andin18 <ndin38397@gmail.com>
2022-05-19 09:17:40 +0700

    andin1811

---
+++ app/Controllers/CobaController.php
@@ -0,0 +1,11 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class CobaController extends Controller{
+    public function index3()
+    {
+        return view ('coba2');
+    }
+}
---
+++ app/Views/welcome_message.php
@@ -0,0 +1,3 @@
+<h6>YAYAYA</h6>
+<h7>DWI ANGGER</h7> <br>
+<h8>ANGGER GORILA</h8>
